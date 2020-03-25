433584370115f3c79c5c7b6a9016fdf4d133702e
Yordan Radev <asofficialasitgets@gmail.com>
2020-03-25 18:14:55 -0400

    Experimented a bit with Vuebar to no avail

--- src/main.js
+++ src/main.js
@@ -3,16 +3,12 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify'
-// import Plotly from 'plotly.js-dist'
 import VueLayers from 'vuelayers'
 import 'vuelayers/lib/style.css' // needs css-loader
-
 
 Vue.use(VueLayers);
 
 Vue.config.productionTip = false
-
-
 
 new Vue({
   router,
